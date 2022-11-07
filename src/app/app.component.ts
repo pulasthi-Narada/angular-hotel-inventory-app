2336f2789ce73be209707f7c76a90d3ca2db8fcb
Pulasthi Narada <64949099+pulasthi-Narada@users.noreply.github.com>
2022-11-07 10:27:34 +0530

    create component and use inline html,css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  //template: `<h1>hi</h1>
+  //  <p>welocome</p> `,
+   styleUrls: ['./app.component.scss'],
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: red;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {
   title = 'hotelInventoryApp';
