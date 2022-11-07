8d4301cba6069c3d55984877cf53d2d4eb1eb5fe
Pulasthi Narada <64949099+pulasthi-Narada@users.noreply.github.com>
2022-11-07 12:52:15 +0530

    directives

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomList, Rooms } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -9,6 +10,18 @@ export class RoomsComponent implements OnInit {
   name: string = 'pulasthi';
 
   hidden: boolean = false;
+
+  rooms: Rooms = {
+    avalibleRooms: 5,
+    totalRooms: 10,
+    bookedRooms: 2,
+  };
+
+  roomList: RoomList[] = [
+    { price: 1500, roomType: 'no ac', roomNumber: 1 },
+    { price: 2500, roomType: 'no ac', roomNumber: 2 },
+    { price: 5000, roomType: ' ac', roomNumber: 3 },
+  ];
 
   constructor() {}
 
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,11 @@
+export interface Rooms {
+  avalibleRooms: number;
+  totalRooms?: number;
+  bookedRooms: number;
+}
+
+export interface RoomList {
+  roomNumber: number;
+  roomType: string;
+  price: number;
+}
